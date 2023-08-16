87a88a93494ba7068ad55241954a6875f41bbe9a
Jacqueline Quissi <102618333+JQuissi@users.noreply.github.com>
2023-08-16 12:14:45 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -6,3 +6,33 @@ FROM DBT.PUBLIC.DBT_RESULTS
 WHERE resource_type = 'model'
     --AND data_formatada = '01-01-1753'
 GROUP BY resource_type, data_formatada;
+
+
+
+WITH StatusCounts AS (
+    SELECT 
+        resource_type,
+        status,
+        TO_CHAR(TO_TIMESTAMP(started_at), 'DD-MM-YYYY') AS data_formatada,
+        COUNT(status) AS total_status,
+        SUM(CASE WHEN status = 'success' THEN 1 ELSE 0 END) AS success_count
+    FROM DBT.PUBLIC.DBT_RESULTS
+    WHERE resource_type = 'model'
+    GROUP BY resource_type, status, data_formatada
+)
+
+SELECT 
+    resource_type,
+    status,
+    total_status,
+    data_formatada,
+    success_count,
+    CASE WHEN total_status = 0 THEN null ELSE success_count::FLOAT / total_status * 100 END AS porcentagem
+FROM StatusCounts;
+
+
+
+
+
+
+
