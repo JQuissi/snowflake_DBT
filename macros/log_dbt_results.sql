642a1f16a911013abf817479bb1254d47f9b3309
Jacqueline Quissi <102618333+JQuissi@users.noreply.github.com>
2023-08-21 10:51:33 -0300

    Update log_dbt_results.sql

--- macros/log_dbt_results.sql
+++ macros/log_dbt_results.sql
@@ -1,10 +1,44 @@
 {% macro log_dbt_results(results) %}
-    -- depends_on: {{ ref('dbt_results') }}
     {%- if execute -%}
         {%- set parsed_results = parse_dbt_results(results) -%}
         {%- if parsed_results | length  > 0 -%}
+
+            {# {%- set source_relation = adapter.get_relation(
+                    database = source('jaffle_shop', 'dbt_results').database,
+                    schema = source('jaffle_shop', 'dbt_results').schema,
+                    identifier = source('jaffle_shop', 'dbt_results').name) -%} #}
+
+            {%- set source_relation = adapter.get_relation(
+                database = "DBT",
+                schema = "public",
+                identifier = "dbt_results") -%}
+
+            {% set table_exists = source_relation is not none %}
+
+            {% if not table_exists %}
+                {% set query_select %}
+
+                    create or replace table dbt_results (
+                        result_id varchar,
+                        invocation_id varchar,
+                        unique_id varchar,
+                        database_name varchar,
+                        schema_name varchar,
+                        name varchar,
+                        resource_type varchar,
+                        status varchar,
+                        message varchar,
+                        execution_time float,
+                        rows_affected int,
+                        started_at varchar
+                    )
+
+                {% endset %}
+                {% set results = run_query(query_select) %}
+            {% endif %}
+            
             {% set insert_dbt_results_query -%}
-                insert into {{ ref('dbt_results') }}
+                insert into dbt.public.dbt_results
                     (
                         result_id,
                         invocation_id,
@@ -14,10 +48,10 @@
                         name,
                         resource_type,
                         status,
+                        message, 
                         execution_time,
                         rows_affected, 
                         started_at
-                        
                 ) values
                     {%- for parsed_result_dict in parsed_results -%}
                         (
@@ -29,16 +63,22 @@
                             '{{ parsed_result_dict.get('name') }}',
                             '{{ parsed_result_dict.get('resource_type') }}',
                             '{{ parsed_result_dict.get('status') }}',
+                            '{{ parsed_result_dict.get('message') | replace("'", "''") }}',
                             {{ parsed_result_dict.get('execution_time') }},
                             {{ parsed_result_dict.get('rows_affected') }},
-                            '{{ parsed_result_dict.get('started_at') }}'
-
+                            CASE 
+                                WHEN '{{ parsed_result_dict.get('started_at') }}' = '1753-01-01' THEN current_timestamp()
+                                ELSE '{{ parsed_result_dict.get('started_at') }}'
+                            END 
                         ) {{- "," if not loop.last else "" -}}
                     {%- endfor -%}
             {%- endset -%}
+            
+            {# Run the insert query #}
             {%- do run_query(insert_dbt_results_query) -%}
         {%- endif -%}
     {%- endif -%}
+    
     -- This macro is called from an on-run-end hook and therefore must return a query txt to run. Returning an empty string will do the trick
     {{ return ('') }}
 {% endmacro %}
