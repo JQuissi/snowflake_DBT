69b0481b44bf0c481ffafc2e4b4e44629300ecc0
Jacqueline Quissi <102618333+JQuissi@users.noreply.github.com>
2023-08-23 15:01:52 -0300

    Update parse_dbt_results.sql

--- macros/parse_dbt_results.sql
+++ macros/parse_dbt_results.sql
@@ -8,7 +8,9 @@
         -- Get the underlying dbt graph node that was executed
         {% set node = run_result_dict.get('node') %}
 
-        {% set rows_affected = run_result_dict.get('adapter_response', {}).get('rows_affected', 0) %}
+        {% set depends_on_nodes = run_result_dict.get('node', {}).get('depends_on', {}).get('nodes', []) %}
+
+        {% set nodes = run_result_dict.get('adapter_response', {}).get('rows_affected', 0) %}
         {%- if not rows_affected -%}
             {% set rows_affected = 0 %}
         {%- endif -%}
@@ -17,8 +19,10 @@
             {% set second_timing = run_result['timing'][0] %} 
             {% set started_at =second_timing['started_at'] %}
         {% else %} 
-            {% set started_at = '1753-01-01 00:00:00.000000'%}
+            {% set started_at = '1753-01-01' %}
         {% endif %}
+
+        {% set message =  run_result_dict.get('message') %} 
 
         {% set parsed_result_dict = {
                 'result_id': invocation_id ~ '.' ~ node.get('unique_id'),
@@ -28,12 +32,18 @@
                 'schema_name': node.get('schema'),
                 'name': node.get('name'),
                 'resource_type': node.get('resource_type'),
-                'status': run_result_dict.get('status'),
-                'execution_time': run_result_dict.get('execution_time'),
+                'status': run_result_dict.get('status'),                
+                'message': message | replace('\'', ''),
+                'execution_time': run_result_dict.get('execution_time'),             
                 'rows_affected': rows_affected,
+                'depends_on_nodes': depends_on_nodes | replace('\'', ''),
                 'started_at': started_at
                 }%}
         {% do parsed_results.append(parsed_result_dict) %}
     {% endfor %}
     {{ return(parsed_results) }}
 {% endmacro %}
+
+
+
+
